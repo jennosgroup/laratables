3a347b37de1e2f32fe618613cc20ba72e4132edb
Jenry Ollivierre <jenry@jenryollivierre.com>
2020-08-25 21:23:34 -0400

    Refactor

--- src/Traits/Sort.php
+++ src/Traits/Sort.php
@@ -163,7 +163,7 @@ trait Sort
      */
     public function getDescSortIconMarkup(): string
     {
-        return '<svg viewBox="0 0 20 20" fill="currentColor" class="arrow-up"><path fill-rule="evenodd" d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
+        return '<svg width="16" height="16" viewBox="0 0 20 20" fill="currentColor" class="arrow-up"><path fill-rule="evenodd" d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
     }
 
     /**
